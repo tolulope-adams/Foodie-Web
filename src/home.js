7d7bb30284988622b61106fd1e35acc6650cb141
T0LUL0PE <tolulopeadams7000@gmail.com>
2022-10-20 22:37:08 +0100

    Add home page header

--- src/home.js
+++ src/home.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 function Home() {
     return <div>
-        Hellloooo
+        <header>
+            <img alt="Foodie Logo"/>
+            <input placeholder="Search for anyhting"/>
+            <select name="languages" id="lang">
+                <option value="javascript">JavaScript</option>
+                <option value="php">PHP</option>
+                <option value="java">Java</option>
+            </select>
+            <button>Search</button>
+            <button>Favorites</button>
+            <button>Notifications</button>
+            <button>Cart</button>
+        </header>
+        <div>
+
+        </div>
+        <footer>
+
+        </footer>
     </div>;
 }
 
