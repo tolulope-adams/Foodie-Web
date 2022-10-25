43136794268a28ba2465332f3c9d506d1f0b4df0
T0LUL0PE <tolulopeadams7000@gmail.com>
2022-10-25 23:45:06 +0100

    Update account.js

---
+++ src/account.js
@@ -0,0 +1,16 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+
+
+function Account() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
+      </>
+  );
+}
+
+export default Account;
